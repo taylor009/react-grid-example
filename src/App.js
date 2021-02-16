dde2523cc56946a0d51d2f4de0ae42c56aee176e
Taylor <taylor@baremetalcloud.com>
2021-02-16 00:19:17 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+// Setting the grid template through props.
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(${props => props.columnNum}, 1fr);
+  grid-template-rows: repeat(${props => props.rowNum}, 1fr);
+  font-size: 50px;
+`;
+
+// I set this as a static array but this could be very dynamic coming from the server.
 function App() {
+    // I set this as a static array but this could be very dynamic coming from the server.
+    const [xGrid] = useState({
+        first: ['X', '.', '.', '.', 'X'],
+        second: ['.', 'X', '.', 'X', '.'],
+        third: ['.', '.', 'X', '.', '.'],
+        fourth: ['.', 'X', '.', 'X', '.'],
+        fifth: ['X', '.', '.', '.', 'X']
+
+    });
+
+    // Destructure from state
+    const { first, second, third, fourth, fifth } = xGrid;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GridContainer className='GridContainer' columnNum={5} rowNum={5}>
+          {first.map((item) => (
+              <p key={item}>{item}</p>
+          ))}
+          {second.map((item) => (
+              <p key={item}>{item}</p>
+          ))}
+          {third.map((item) => (
+              <p key={item}>{item}</p>
+          ))}
+          {fourth.map((item) => (
+              <p key={item}>{item}</p>
+          ))}
+          {fifth.map((item) => (
+              <p key={item}>{item}</p>
+          ))}
+      </GridContainer>
     </div>
   );
 }
